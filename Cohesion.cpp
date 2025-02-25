a1e97cbcfb9c9ae2960ee0fa529f040e61fd25f2
Michael L. Collard <collard@uakron.edu>
2025-02-25 14:43:25 -0500

    Add cohesion logical

--- Cohesion.cpp
+++ Cohesion.cpp
@@ -39,5 +39,12 @@ int main() {
         double testAverage;
     }
 
+    // @cohesion Logical
+    {
+        std::vector<std::string> testNames, quizNames, exerciseNames;
+        std::vector<int> exercises, quizzes, tests;
+        double exerciseAverage, quizAverage, testAverage;
+    }
+
     return 0;
 }
